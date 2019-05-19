47d57b91e33ff78c158691022b2a9724a82cddc0
Ezard <ezard.ben@gmail.com>
2019-05-19 17:27:44 +0100

    Add CompaniesHouseApi class, and link in company profile retrieval

---
+++ src/api.ts
@@ -0,0 +1,17 @@
+import * as request from "superagent";
+import { Profile } from "./profile";
+
+export class CompaniesHouseApi {
+	readonly profile = new Profile(this);
+
+	constructor(private readonly apiKey: string) {
+	}
+
+	async get<T>(path: string, query: object = {}): Promise<T> {
+		const response = await request
+			.get(`https://api.companieshouse.gov.uk${path}`)
+			.query(query)
+			.set({ "Authorization": `Basic ${this.apiKey}` });
+		return response.body as T;
+	}
+}
